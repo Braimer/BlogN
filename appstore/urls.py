9540ee9cdc4ecca1de69237bb7aa47c7c9b259eb
Braimer <omondibraimer8@gmail.com>
2025-04-26 05:48:02 +0300

    views and templates updated

--- appstore/urls.py
+++ appstore/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
-    path("home/",views.home,name="home"),
+    path('', views.post_list, name='post_list'),
+    path('post/<slug:slug>/', views.detail, name='detail'),
+    path('tag/<str:tag_name>/', views.tag_posts, name='tag_posts'),
+    path('profile/<str:username>/', views.profile, name='profile'),
 ]
+   
--- appstore/views.py
+++ appstore/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
-from .models import Tag,Post
+from django.shortcuts import render,get_object_or_404
+from .models import Tag,Post,Profile
 from django.views import generic       #for class based views
 
 # Create your views here.
 
 #home view ,shows posts
-def home(request):
+'''def home(request):
     posts=Post.objects.all()
-    return render(request,'home.html',{'posts':posts})
+    return render(request,'home.html',{'posts':posts})'''
 
+
+
+def post_list(request):
+    posts = Post.objects.filter(published=True)
+    return render(request, 'post_list.html', {'posts': posts})
+
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, published=True)
+    return render(request, 'detail.html', {'post': post})
+
+def tag_posts(request, tag_name):
+    tag = get_object_or_404(Tag, name=tag_name)
+    posts = Post.objects.filter(tags=tag, published=True)
+    return render(request, 'tag_posts.html', {'tag': tag, 'posts': posts})
+
+def profile(request, username):
+    profile = get_object_or_404(Profile, user__username=username)
+    posts = Post.objects.filter(author=profile, published=True)
+    return render(request, 'profile.html', {'profile': profile, 'posts': posts})
